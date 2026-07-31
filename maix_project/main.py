8885c5c16b4fd6b65b4e8a50cbbe806d71bd05df
yiqing <yiqing19817158771@outlook.com>
2026-08-01 06:33:28 +0800

    feat: Convert 5-frame average X-axis speed unit to px/ms (formatted to 3 decimal places)

--- maix_project/main.py
+++ maix_project/main.py
@@ -109,7 +109,7 @@ def send_tracking(port, x, vx_5f):
     if x == -1 and vx_5f == -1:
         packet = "$-1,-1\r\n"
     else:
-        packet = "${},{}\r\n".format(x, vx_5f)
+        packet = "${},{:.3f}\r\n".format(x, vx_5f)
     if port is not None:
         port.write_str(packet)
     if SERIAL.get("print_tx", True):
@@ -323,7 +323,7 @@ def main():
                         smooth_alpha * raw_y + (1.0 - smooth_alpha) * filtered[1],
                     )
 
-                # --- 5-Frame Sliding Window Average X-Speed Calculation (px/s) ---
+                # --- 5-Frame Sliding Window Average X-Speed Calculation (px/ms) ---
                 history_5f.append((filtered[0], now_ms))
                 if len(history_5f) > 5:
                     history_5f.pop(0)
@@ -331,19 +331,19 @@ def main():
                 if len(history_5f) == 5:
                     old_x, old_t = history_5f[0]
                     cur_x, cur_t = history_5f[-1]
-                    dt_s = time.ticks_diff(cur_t, old_t) / 1000.0
-                    if dt_s > 0.01:
-                        vx_5f = int(round((cur_x - old_x) / dt_s))
+                    dt_ms = time.ticks_diff(cur_t, old_t)
+                    if dt_ms > 10:
+                        vx_5f = (cur_x - old_x) / float(dt_ms)
                     else:
-                        vx_5f = 0
+                        vx_5f = 0.0
                 else:
-                    vx_5f = 0
+                    vx_5f = 0.0
 
                 output_x = int(round(filtered[0]))
                 output_y = int(round(filtered[1]))
                 out_x, out_y = output_x, output_y
 
-                # Send absolute X position and 5-frame average X-speed over serial
+                # Send absolute X position and 5-frame average X-speed (px/ms) over serial
                 send_tracking(port, output_x, vx_5f)
 
                 img.draw_rect(target.x, target.y, target.w, target.h,
@@ -353,7 +353,7 @@ def main():
                 img.draw_string(
                     target.x,
                     max(0, target.y - 20),
-                    "ball {:.2f} x={} vx={}px/s".format(target.score, output_x, vx_5f),
+                    "ball {:.2f} x={} vx={:.3f}px/ms".format(target.score, output_x, vx_5f),
                     color=image.COLOR_GREEN,
                 )
 
